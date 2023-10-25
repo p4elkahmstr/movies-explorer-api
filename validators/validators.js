502eea3f0925f9d19f87568931daba4457c3b4dc
Savvinov Ivan <57740348+morningjacketup@users.noreply.github.com>
2023-10-25 22:07:35 +0300

    change duration to number type

--- validators/validators.js
+++ validators/validators.js
@@ -6,7 +6,7 @@ const createMovieValidator = celebrate({
     body: Joi.object().keys({
         country: Joi.string().required(),
         director: Joi.string().required(),
-        duration: Joi.string().required(),
+        duration: Joi.number().required(),
         year: Joi.string().required(),
         description: Joi.string().required(),
         image: Joi.string().required().regex(urlRegex),
